922634af71fe09f99c1570b50d2f81e3d58853e8
jasmineparvatham <jasmine@gmail.com>
2026-02-06 18:18:49 +0530

    connect deployed server

--- web/src/api/client.js
+++ web/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_BASE =
-  import.meta.env.VITE_API_BASE || "http://127.0.0.1:8000/api";
+  import.meta.env.VITE_API_BASE || "https://chemviz-fosseebackend-junnu.onrender.com/api";
 
 export function createClient(getToken) {
   const client = axios.create({ baseURL: API_BASE });
